a268b41b04dbe90560d4d7b3c82468eaf04b64b7
Malek Rezgui <m.rezgui@outlook.fr>
2025-02-25 15:58:32 +0100

    :chart_with_upwards_trend: Add vercel SpeedInsights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import '../styles/global.css'
 import 'katex/dist/katex.css'
 import Footer from '../components/footer'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { useRouter } from 'next/router'
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
   return (
     <>
       <Component {...pageProps} />
       <Footer />
+      <SpeedInsights route={router.pathname} />
     </>
   )
 }
