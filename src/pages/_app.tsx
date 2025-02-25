2f429487d910ce59a753e88a621cb96e7031fd44
Malek Rezgui <m.rezgui@outlook.fr>
2025-02-25 16:04:35 +0100

    :chart_with_upwards_trend: Add vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/global.css'
 import 'katex/dist/katex.css'
 import Footer from '../components/footer'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 import { useRouter } from 'next/router'
 
 export default function MyApp({ Component, pageProps }) {
@@ -11,6 +12,8 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Component {...pageProps} />
       <Footer />
+
+      <Analytics />
       <SpeedInsights route={router.pathname} />
     </>
   )
